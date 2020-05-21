74f6c2ce8896a7d30378b252d3ef9e1f2219b1ce
Huseyin Saglam <hsaglam001@gmail.com>
2020-05-22 00:26:54 +0300

    SpringSecurity-Jwt-Mysql

---
+++ jwt-mysql-security/src/main/java/com/example/auth/JwtUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.example.auth;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.UserDao;
+import com.example.model.DAOUser;
+import com.example.model.UserDTO;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		DAOUser user = userDao.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				new ArrayList<>());
+	}
+
+	public DAOUser save(UserDTO user) {
+		DAOUser newUser = new DAOUser();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+		return userDao.save(newUser);
+	}
+}
---
+++ jwt-mysql-security/src/main/java/com/example/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.example.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.model.DAOUser;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer>{
+
+	DAOUser findByUsername(String username);
+}
